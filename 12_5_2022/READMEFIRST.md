beb4251ba2a593bb5b3dd60e51e0b0240c917647
michael <michaelsanchez8815@gmail.com>
2022-12-05 14:18:37 -0600

    Created 12_5_2022

--- 12_5_2022/READMEFIRST.md
+++ 12_5_2022/READMEFIRST.md
@@ -1,17 +1,10 @@
 **Directions**
 
 1. Have Node installed on your computer.
-2. When you open to the folder, please run **npm install**
+2. When you open to the folder of the day, please run **npm install**
 3. Work incrementally from Mild to Hot.
 4. Please read the **commented out directions** to know what you are supposed to create.
    1. For example: You need to create specific named functions.
-5. To **RUN** your code: You must type in terminal:
-   1. Mild: node mild/mild.js
-   2. Medium: node medium/medium.js
-   3. Hot: node hot/hot.js
-6. To **TEST** your code: You must type in terminal:
-   1. Mild: **npm run mild-test**
-   2. Medium: **npm run medium-test**
-   3. Hot: **npm run hot-test**
-   4. All Tests: **npm test**
-   5. Tests run after every save: **npm run test:watch**
+5. To **RUN** your code: You must type in terminal (as long as your in the correct folder -> 12_5_2022): node index
+
+6. To **TEST** your code: You must type in terminal: 4. All Tests: **npm test** 5. Tests run after every save: **npm run test:watch**
---
+++ 12_5_2022/tests/doNotLookHere.js
@@ -0,0 +1,103 @@
+export const employees = [
+  {
+    name: 'Michael',
+    job: 'Owner',
+    salary: 500000,
+  },
+  {
+    name: 'Lisa',
+    job: 'CEO',
+    salary: 200000,
+  },
+  {
+    name: 'Lily',
+    job: 'CTO',
+    salary: 150000,
+  },
+  {
+    name: 'Mila',
+    job: 'CFO',
+    salary: 89975,
+  },
+  {
+    name: 'Remi',
+    job: 'Lead Developer',
+    salary: 75000,
+  },
+  {
+    name: 'Kenzie',
+    job: 'Junior Developer',
+    salary: 50499,
+  },
+  {
+    name: 'Alec',
+    job: 'Designer',
+    salary: 35499,
+  },
+  {
+    name: 'Emma',
+    job: 'Designer',
+    salary: 35499,
+  },
+  {
+    name: 'Missy',
+    job: 'Full-Stack Developer',
+    salary: 250000,
+  },
+  {
+    name: 'Nico',
+    job: 'Product Owner',
+    salary: 170000,
+  },
+];
+
+export const reverseAZ = [
+  { name: 'Remi', job: 'Lead Developer', salary: 75000 },
+  { name: 'Nico', job: 'Product Owner', salary: 170000 },
+  { name: 'Missy', job: 'Full-Stack Developer', salary: 250000 },
+  { name: 'Mila', job: 'CFO', salary: 89975 },
+  { name: 'Michael', job: 'Owner', salary: 500000 },
+  { name: 'Lisa', job: 'CEO', salary: 200000 },
+  { name: 'Lily', job: 'CTO', salary: 150000 },
+  { name: 'Kenzie', job: 'Junior Developer', salary: 50499 },
+  { name: 'Emma', job: 'Designer', salary: 35499 },
+  { name: 'Alec', job: 'Designer', salary: 35499 },
+];
+
+export const over100kEmployees = ['Michael', 'Lisa', 'Lily', 'Missy', 'Nico'];
+
+export const salaryLeastToGreatest = [
+  { name: 'Alec', job: 'Designer', salary: 35499 },
+  { name: 'Emma', job: 'Designer', salary: 35499 },
+  { name: 'Kenzie', job: 'Junior Developer', salary: 50499 },
+  { name: 'Remi', job: 'Lead Developer', salary: 75000 },
+  { name: 'Mila', job: 'CFO', salary: 89975 },
+  { name: 'Lily', job: 'CTO', salary: 150000 },
+  { name: 'Nico', job: 'Product Owner', salary: 170000 },
+  { name: 'Lisa', job: 'CEO', salary: 200000 },
+  { name: 'Missy', job: 'Full-Stack Developer', salary: 250000 },
+  { name: 'Michael', job: 'Owner', salary: 500000 },
+];
+
+export const bonusesAdded = [
+  { name: 'Michael', job: 'Owner', salary: 500000, bonus: 50000 },
+  { name: 'Lisa', job: 'CEO', salary: 200000, bonus: 20000 },
+  { name: 'Lily', job: 'CTO', salary: 150000, bonus: 15000 },
+  { name: 'Mila', job: 'CFO', salary: 89975, bonus: 4498.75 },
+  { name: 'Remi', job: 'Lead Developer', salary: 75000, bonus: 3750 },
+  {
+    name: 'Kenzie',
+    job: 'Junior Developer',
+    salary: 50499,
+    bonus: 2524.95,
+  },
+  { name: 'Alec', job: 'Designer', salary: 35499, bonus: 1774.95 },
+  { name: 'Emma', job: 'Designer', salary: 35499, bonus: 1774.95 },
+  {
+    name: 'Missy',
+    job: 'Full-Stack Developer',
+    salary: 250000,
+    bonus: 25000,
+  },
+  { name: 'Nico', job: 'Product Owner', salary: 170000, bonus: 17000 },
+];
